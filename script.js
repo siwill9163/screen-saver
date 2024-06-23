fa0d4b997450358f20454135e6366e54b86449d5
Simon <143303065+siwill9163@users.noreply.github.com>
2024-06-23 04:50:53 +0100

    Update script.js
    done at the office

--- script.js
+++ script.js
@@ -4,25 +4,31 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height= window.innerHeight;
-ctx.fillStyle = 'red';
+
 console.log(ctx);
+const gradient = ctx.createLinearGradient(0,0,canvas.width, canvas.height);
+gradient.addColorStop(0, 'white');
+gradient.addColorStop(0.5, 'magenta');
+gradient.addColorStop(1, 'blue');
+ctx.fillStyle = gradient;
+ctx.strokeStyle = "white";
 
 class Particle{
     constructor(effect){
         this.effect = effect;
-        this.radius = Math.random() * 40 + 5;
+        this.radius = Math.random() * 5 + 2;
         this.x = this.radius + Math.random() * (this.effect.width - this.radius * 2);
         this.y =this.radius + Math.random() * (this.effect.height - this.radius * 2);
-        this.vx = Math.random() *  4 - 2;
-        this.vy = Math.random() *  4 - 2;
+        this.vx = Math.random() *  1 - 0.5;
+        this.vy = Math.random() *  1 - 0.5;
         
     }
     draw(context){
-        context.fillStyle = 'hsl(' + this.x * 0.5 + ', 100%, 50%)';
+        //context.fillStyle = 'hsl(' + this.x * 0.5 + ', 100%, 50%)';
         context.beginPath();
         context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
         context.fill();
-        context.stroke();
+        //context.stroke();
     }  
     update(){
         this.x += this.vx;
@@ -50,10 +56,33 @@ class Effect{
        }
     }
     handleParticales(context){
+        this.connectParticales(context);
         this.particle.forEach(particle => {
             particle.draw(context);
             particle.update();
         });
+        
+    }
+    connectParticales(context){
+        const maxDistance = 100;
+        for(let a = 0; a < this.particle.length; a++){
+            for(let b = a; b < this.particle.length; b++){
+                const dx = this.particle[a].x - this.particle[b].x;
+                const dy = this.particle[a].y - this.particle[b].y;
+                const distance = Math.hypot(dx, dy);
+            if (distance < maxDistance){
+                context.save();
+                const opacity = 1 - (distance/maxDistance);
+                context.globalAlpha = opacity;
+                context.beginPath();
+                context.moveTo(this.particle[a].x, this.particle[a].y);
+                context.lineTo(this.particle[b].x, this.particle[b].y);
+                context.stroke();
+                context.restore();
+            }
+            }
+        }
+           
     }
 }
 const effect = new Effect(canvas);
